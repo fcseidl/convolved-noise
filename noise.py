1f595fcc8ae066c22fcb90a42a02bfc52cac79cc
fcseidl <fcseidl@umich.edu>
2025-09-20 08:18:18 -0700

    automatic inference of effective range

--- noise.py
+++ noise.py
@@ -9,9 +9,56 @@ def distfromcenter(shape):
     Create an array of the requested shape where each index holds its
     distance from the center index.
     """
-    grid = np.ogrid[*(slice(0, s) for s in shape), ]
+    grid = np.ogrid[*(slice(0, s) for s in shape),]
     d2 = sum((g - s / 2 + 0.5) ** 2 for g, s in zip(grid, shape))
     return np.sqrt(d2)
+
+
+def bisection(
+        f: Callable[[float], float],
+        a: float,
+        b: float,
+        atol: float = 1e-6,
+        fa=None,
+        fb=None
+) -> float:
+    """
+    Use the bisection method to estimate a root of f on the interval [a, b]. The root must exist and be unique.
+
+    :param f: function for rootfinding
+    :param a: lower bound
+    :param b: upper bound
+    :param atol: absolute tolerance
+    :param fa: optionally f(a)
+    :param fb: optionally f(b)
+    :return: the approximate root, or None if no root is found
+    """
+    m = (a + b) / 2
+    fa = fa if fa is not None else f(a)
+    fb = fb if fb is not None else f(b)
+    fm = f(m)
+    # base case: return mid- or endpoint closest to root
+    if b - a < atol:
+        return [a, m, b][np.argmin(np.abs([fa, fm, fb]))]
+    # recursive case: search for root on subinterval
+    if fa * fm < 0:
+        return bisection(f, a=a, b=m, atol=atol, fa=fa, fb=fm)
+    if fm * fb <= 0:
+        return bisection(f, a=m, b=b, atol=atol, fa=fm, fb=fb)
+
+
+# parameters for eff range search
+_ftol = 1e-4
+_eff_range_max = 1.
+
+
+def estimate_effective_range(kernel: np.ufunc) -> float:
+    """Return an estimate for the effective range of a kernel, or throw an exception."""
+    result = bisection(lambda x: kernel(x) - _ftol, 0, _eff_range_max)
+    if result is None:
+        raise ValueError("Unable to infer effective range. This is only guaranteed for "
+                         + f"monotonically decreasing kernels satisfying k({_eff_range_max}) < {_ftol}.")
+    return result
 
 
 def convolve(x, y):
@@ -24,60 +71,28 @@ def convolve(x, y):
     return irfftn(fx * fy)
 
 
-def cone_filter(radius, resolution, dimension) -> np.ndarray:
-    """
-    Convolutional filter for cone kernel, scaled to unit norm.
-
-    :param radius: Radius of cone.
-    :param resolution: Number of grid cells per unit.
-    :param dimension: Dimension of filter.
-    """
-    hw = int(radius * resolution)
-    w = 2 * hw + 1
-    shape = tuple(w for _ in range(dimension))
-    d = distfromcenter(shape)
-    filt = (d < radius)
-    return filt / np.linalg.norm(filt)
-
-
-def rbf_filter(sigma, nsig, resolution, dimension) -> np.ndarray:
-    """
-    Convolutional filter for RBF kernel, scaled to unit norm.
-
-    :param sigma: Sigma paremeter of radial basis function. (Not squared.)
-    :param nsig: Half-width of filter in units of sigma.
-    :param resolution: Number of grid cells per unit.
-    :param dimension: Dimension of filter.
-    """
-    hw = int(sigma * nsig * resolution)
-    w = 2 * hw + 1
-    shape = tuple(w for _ in range(dimension))
-    d2 = distfromcenter(shape) ** 2
-    filt = np.exp(-d2 / (sigma * sigma))  # convolution of Gaussian is Gaussian
-    return filt / np.linalg.norm(filt)
-
-
 def noise(
         shape: float | tuple,
         kernel: np.ufunc,
-        eff_range: float,
+        eff_range: float | None = None,
         channel_cov: float | np.ndarray = 1.,
         periodic: bool | tuple = False,
         seed: int = None
 ) -> np.ndarray:
     """
-    Sample a mean-zero Gaussian process over a box in n-dimensional space, satisfying
-        Cov(N(x), N(y)) = (k * k)(||x - y||),
-    where N is the process, k is a kernel function, and * represents the
-    (discrete) convolution operator.
+    Sample a mean-zero Gaussian process over a box in n-dimensional space.
+    If N is the process, k is a kernel function, and * represents the
+    (discrete) convolution operator, then Cov(N(x), N(y)) is proportional
+    to (k * k)(||x - y||) for all sample locations x and y.
 
     :param shape: Shape of grid in which to sample noise. The grid resolution
                     is set so that the length along the first axis is 1. For
                      instance, shape=np.ones(d) gives a unit hypercube in d
                      dimensions.
-    :param kernel: Convolutional kernel used to define noise covariance. Must be broadcastable.
+    :param kernel: Convolutional kernel used to define noise autocorrelation. Must be broadcastable.
     :param eff_range: Effective range of kernel. Longer-range correlations are reduced or
-                        absent in the noise simulated.
+                        absent in the noise simulated. If not provided, the effective range
+                        inferred automatically assuming a monotonically decreasing kernel.
     :param channel_cov: Covariance matrix of channels in the noise sample. Provide a
                             positive scalar for single-channel noise. Otherwise, provide
                             a symmetric positive definite matrix with a row for each
@@ -95,6 +110,10 @@ def noise(
     if type(periodic) == bool:
         periodic = [periodic for _ in shape]
 
+    # infer effective range if needed
+    if eff_range is None:
+        eff_range = estimate_effective_range(kernel)
+
     # construct kernel to convolve with
     d = distfromcenter([2 * eff_range * shape[0] + 1 for _ in shape])
     d /= shape[0]
@@ -105,23 +124,24 @@ def noise(
     # determine shape of white noise to sample
     pad_shape = shape.copy()
     pad_shape[np.equal(periodic, False)] += filt.shape[0] - 1
-
     channel_cov = np.atleast_2d(channel_cov)
     n_channels = channel_cov.shape[0]
-    cholesky_factor = np.linalg.cholesky(channel_cov)
+    result = np.empty(tuple(shape) + (n_channels,))
 
-    result = np.empty(tuple(shape) + (n_channels, ))
-
+    # sample and smooth noise channels
     for c in range(n_channels):
         white = np.random.randn(*pad_shape)
         smooth = convolve(white, filt)
-        result[..., c] = smooth[*(slice(0, sj) for sj in shape), ]
+        result[..., c] = smooth[*(slice(0, sj) for sj in shape),]
 
+    # induce channel correlations
+    cholesky_factor = np.linalg.cholesky(channel_cov)
     return result @ cholesky_factor.T
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+
 
     # based on SO: https://stackoverflow.com/a/457805/14149906
     def erf(x):
@@ -142,20 +162,20 @@ if __name__ == "__main__":
         y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * np.exp(-x * x)
         return sign * y  # erf(-x) = -erf(x)
 
-    crg, crb, cgb = 0.0, 0.99, 0.0
+
+    crg, crb, cgb = 0.0, 0.0, 0.9
     rgb = noise(
         shape=(800, 1000),
-        kernel=lambda x: np.ones_like(x),
-        eff_range=0.2,
-        periodic=False,     # [False, False],
+        kernel=lambda x: 0.2 - x,  # np.ones_like(x),eff_range=0.2,  # todo: make this optional
+        periodic=False,  # [False, False],
         channel_cov=np.array([
-                        [1, crg, crb],
-                        [crg, 1, cgb],
-                        [crb, cgb, 1]
-                    ])
+            [1, crg, crb],
+            [crg, 1, cgb],
+            [crb, cgb, 1]
+        ])
     )
 
-    rgb = 0.5 * (1 + erf(rgb))      # inverse cdf (approximated)
+    rgb = 0.5 * (1 + erf(rgb))  # inverse cdf (approximated)
 
     rgb = np.roll(rgb, (100, 100), axis=(0, 1))
 
